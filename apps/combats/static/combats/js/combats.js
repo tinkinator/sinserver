f70f088cf763a59815fbcdad7ac5161365d00d95
Tink <solamyilly@gmail.com>
2017-01-16 22:48:45 -0800

    now with skulls!

--- apps/combats/static/combats/js/combats.js
+++ apps/combats/static/combats/js/combats.js
@@ -5,4 +5,14 @@ $(document).ready(function(){
     var topTenTable = $('#topTen').DataTable({"pageLength": 5});
     $('[data-toggle="tooltip"]').tooltip({container: 'body'});
     $('#topTen_wrapper > .row:nth-child(odd) > div').removeClass('col-sm-6').addClass('col-sm-3');
-})
+    var plStatsEl = $('#topTen .player .pl-name').next();
+    console.log(plStatsEl);
+    plStatsEl.each(function(){
+        var killed = parseInt($(this).find('.killed').text());
+        var total = parseInt($(this).find('.total').text());
+        if(total - killed == 0){
+            $('<div class="skull-pic"></div>').appendTo($(this).prev());
+        }
+    })
+
+});
